bb541ea3855669d3a4a62371dc6fe1bd266fb77f
Filip Ćuić <filipcuic.nasice@gmail.com>
2019-07-18 15:00:40 +0200

    frontend wireframe Angular

---
+++ src/frontend/angular template/angular-crash-todolist/src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Input,EventEmitter,Output} from '@angular/core';
+import {Todo} from '../models/Todo'
+import {TodoService} from '../../services/todo.service';
+
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+
+  @Input() todo : Todo;
+  @Output() deleteTodo:EventEmitter<Todo>= new EventEmitter();
+  constructor(private todoService:TodoService ) { }
+
+  ngOnInit() {
+  }
+  //Setting dynamic classes
+  setClasses(){
+    let classes={
+      todo:true,
+      'is-complete':this.todo.completed
+    }
+    return classes;
+  }
+  onToggle(todo){
+    //toggle na stranici
+    todo.completed=!todo.completed;
+    //toggle na serveru
+    this.todoService.toggleCompleted(todo).subscribe(todo=>
+      console.log(todo));
+  }
+  onDelete(todo){
+  this.deleteTodo.emit(todo);
+  }
+}
