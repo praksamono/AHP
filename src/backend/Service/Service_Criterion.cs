d724d6d7cce8eb2838f38a8ff74a4d9d48fb5652
marko-grozdanic <marko.grozdanic27@gmail.com>
2019-07-18 15:25:12 +0200

    Initialised service classes

---
+++ src/backend/Service/Service_Criterion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AHP_Service
+{
+    class Service_Criterion
+    {
+        //
+    }
+}
