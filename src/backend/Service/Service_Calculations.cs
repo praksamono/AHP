df965b50d0e7e0a814e2bc2bae8a8cfb8b8192ad
marko-grozdanic <marko.grozdanic27@gmail.com>
2019-07-19 11:56:07 +0200

    Service Layer calculation and matrix placeholder methods

---
+++ src/backend/Service/Service_Calculations.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AHP_Service
+{
+    class Service_Calculations
+    {
+        public List<float> CalculatePriorities(List<List<float>> Matrix)
+        {
+            return null;
+        }
+            
+    }
+}
