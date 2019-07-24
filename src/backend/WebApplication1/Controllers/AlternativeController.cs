5c8b2047659dfda23af26c250629f83ed6004f22
mpatajac <patajacmatija3@gmail.com>
2019-07-24 10:47:21 +0200

    Add Alternative controller

---
+++ src/backend/WebApplication1/Controllers/AlternativeController.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using AHP.Service.Common;
+using Model.Common;
+
+namespace WebAPI
+{
+	[Route("api/alternatives")]
+	[ApiController]
+	public class AlternativeController : ControllerBase
+	{
+		private readonly IAlternativeService _service;
+		private readonly IMapper _mapper;
+
+		public AlternativeController(IAlternativeService service, IMapper mapper)
+		{
+			_service = service;
+			_mapper = mapper;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<List<AlternativeDTO>>> GetAlternativesAsync(Guid id)
+		{
+			var alternatives = await _service.GetAlternativesAsync();
+
+			if (alternatives == null)
+			{
+				return NotFound();
+			}
+			return Ok(_mapper.Map<List<AlternativeDTO>>(alternatives));
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<List<IAlternative>>> CreateAlternativesAsync(List<AlternativeDTO> alternatives)
+		{
+			foreach (var alternative in alternatives)
+			{
+				if (string.IsNullOrEmpty(alternative.AlternativeName))
+				{
+					return BadRequest(new {message = "Alternative name is not set."});
+					// throw new HttpResponseException("Alternative name is not set.", HttpStatusCode.BadRequest);
+				}
+			}
+
+			var mappedAlts = _mapper.Map<List<IAlternative>>(alternatives);
+			await _service.CreateAlternativesAsync(mappedAlts);
+
+			return CreatedAtAction(nameof(GetAlternativesAsync), new { }, mappedAlts);
+		}
+
+	}
+
+	public class AlternativeDTO
+	{
+		public string AlternativeName { get; set; }
+		public float AlternativeGlobalPriority { get; set; }
+		public List<ICriteriumAlternative> criteriumAlternatives { get; set; }
+	}
+
+}
