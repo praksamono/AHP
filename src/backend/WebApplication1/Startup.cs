63a5caf75847d5fe438cd78be790513ae131bcc0
Kova <jakkovac@gmail.com>
2019-07-31 13:00:31 +0200

    Added CORS policy for Angular

--- src/backend/WebApplication1/Startup.cs
+++ src/backend/WebApplication1/Startup.cs
@@ -43,6 +43,8 @@ namespace WebApplication1
 
             services.AddDbContextPool<AHPContext>(options => options.UseSqlServer(this.Configuration.GetConnectionString("connectionString")));
 
+            services.AddCors();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             var config = new MapperConfiguration(cfg =>
@@ -77,6 +79,12 @@ namespace WebApplication1
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(builder =>
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader()
+            );
 
             app.UseMvc();
         }
